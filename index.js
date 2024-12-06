fc4254f13366957d137dc1943eaf5cb7980e9dff
toadkiller24 <sikhumbuzonxumalo24@gmail.com>
2024-12-06 13:01:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require('axios');
 const app = express();
-const port = 5001;
+const port = 5002;
 const ethers = require('ethers');
 const abi = require('./abi.json');
 
